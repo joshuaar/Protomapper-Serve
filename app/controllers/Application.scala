d0dc4acd48830987e021fcd959b6b9adf1e72756
root <joshamosricher@gmail.com>
2014-04-01 23:21:07 +0000

    After streaming content, close DirectoryReader

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,4 +1,5 @@
 package controllers 
+import scala.concurrent.ExecutionContext.Implicits.global
 import com.protomapper.compile._
 import com.protomapper.update._
 import com.protomapper.search._
@@ -7,6 +8,7 @@ import org.apache.lucene.store.NIOFSDirectory
 import java.io.File
 import play.api._
 import play.api.mvc._
+import play.api.libs.iteratee.Enumerator
 
 object Application extends Controller {
   val parser = new PatternParser
@@ -15,7 +17,7 @@ object Application extends Controller {
   val access = new LuceneAccess(ix)
   val search = new Searcher(compiler,access)
   def index = Action {
-    Ok(views.html.index("Your new application asds ready."))
+    Ok(views.html.index("Protomapper ready"))
   }
   
   def query(q:String,r:String) = Action {
@@ -25,6 +27,26 @@ object Application extends Controller {
     val res = search.search(q)
     val out = res.getJSON(from.toInt, to.toInt)
     Ok(out)
+  }
+  
+  def download(q:String) = Action {
+    val res = search.search(q)
+    val (data,reader) = res.getFastaStream()
+    val dataBytes = data.view.map(a=>a.getBytes()).toIterator
+    class ClosableIterator extends Iterator[Array[Byte]]{
+        def hasNext():Boolean = {
+            val ret = dataBytes.hasNext
+            if(!ret)
+                reader.close()
+            ret
+        }
+        def next():Array[Byte] = {
+            dataBytes.next()
+        }
+    }
+    val toEnumerate = new ClosableIterator()
+    val dataContent:Enumerator[Array[Byte]] = Enumerator.enumerate(toEnumerate)
+    Ok.stream(dataContent.andThen({Enumerator.eof}))
   }
   
   def summary(q:String) = Action {
